1ce44b5e684c2f6bf276720959f5cbde50f4dd73
BLUEBEAM\bstilley <bstilley@bluebeam.com>
2017-08-17 19:10:51 -0700

    Fix broken HMR on windows

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 function transform(source, map) {
+    const resourcePath = JSON.stringify(this.resourcePath);
+
     const prependText = `/** @jsx Declarity.createEntity */`;
 
     const appendText = `
 if (module.hot) {
-    const next = require('${this.resourcePath}')
+    console.log('resourcePath', ${resourcePath})
+    const next = require(${resourcePath})
 
     if (!global.__DECLARITY_HOT_LOADER__) {
         global.__DECLARITY_HOT_LOADER__ = {}
         console.log('Declarity hot loader is active')
     }
-    global.__DECLARITY_HOT_LOADER__['${this.resourcePath}'] = next;
-    module.exports.default.__declarity_location = '${this.resourcePath}'
+    global.__DECLARITY_HOT_LOADER__[${resourcePath}] = next;
+    module.exports.default.__declarity_location = ${resourcePath}
     module.exports.default.__declarity_id = window.crypto.getRandomValues(new Uint8Array(20))
 }
 `;
